2403e81ddae97d4069fb1e5d29242cf61092e372
randymxd06 <randym0624@gmail.com>
2024-06-14 09:46:11 -0400

    feat: :sparkles: I'm going to start creating the microservice

---
+++ src/common/index.ts
@@ -0,0 +1,2 @@
+export * from './dto/pagination.dto';
+export * from './exceptions/rpc_custom_exception.filter';
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { envs } from './config/envs';
+import { RpcCustomExceptionFilter } from './common';
 
 async function bootstrap() {
+
+  const logger = new Logger('Main Client Gateway');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.useGlobalFilters(new RpcCustomExceptionFilter())
+
+  await app.listen(envs.port);
+
+  logger.log(`Client Gateway running on port: ${envs.port}`)
+
 }
 bootstrap();
